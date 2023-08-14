c61be59b23210813a2b0e5de770b65b06394b377
Shantal Eslava <mseg006@gmail.com>
2023-08-13 21:41:18 -0600

    validate y stats sirven y arrojan datos correspondientes y combinados

--- cli.js
+++ cli.js
@@ -5,20 +5,36 @@ const relativePath = process.argv[2]; // Renombramos relativePath a filePath par
 
 mdLinks(relativePath).then(response => {
     
-    if (process.argv.includes('--validate')) {
-        response.forEach(link => {
-            axios.get(link.url).then(response =>{
-                
-                if (response.status >= 200 && response.status < 300) {
-                  console.log({...link, status: response.status})
-              }}).catch(error => {
-                console.log({...link, status: 404})
+let broken = 0
+let validate = process.argv.includes('--validate' )
+let stats = process.argv.includes('--stats' )
 
-              });
+    if ( validate || stats) {
+        let promesas = response.map(link => {
+            return axios.get(link.url).then(response =>{
+                
+                if (response.status >= 200 && response.status < 300 && validate && !stats ) { 
+                    console.log({...link, status: response.status})
+                 }}).catch(error => {
+                if(validate && !stats){ console.log({...link, status: 404}) }
+                broken++
+            });
         })
-    } else if(process.argv.includes('--stats')) {
+        Promise.all(promesas).then(_ =>{
+            if (stats && validate){
+                console.log('broken: ' + broken)
+            }
+        })
+    } 
+    if(stats) {
         console.log(response.length)
-    }else {
-    console.log(response)}
+        //aqui va el set
+        let unicos = new Set(response.map((link) =>link.url)).size 
+        console.log(unicos)
+
+    }
     
 })
+//new Set(links.map((link) => link.href)).size
+//total unicos rotillos
+//re
